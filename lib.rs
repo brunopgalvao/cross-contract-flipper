ee7bc2833ce517f95d121ef60d97cebc3b8d0e42
Bruno Galvao <brunopgalvao@gmail.com>
2024-08-22 11:29:06 -0400

    update code

--- lib.rs
+++ lib.rs
@@ -5,12 +5,17 @@
 
 #[ink::contract]
 mod cross_contract_flipper {
+    /// The global call builder type for an ink! trait definition.
+    /// Allows us to use call_mut() which is a method of TraitCallBuilder
+    /// that returns an exclusive reference to the global call builder type.
     use ink::codegen::TraitCallBuilder;
-    use other_contract::OtherContractRef;
+
     use ink::env::{
-        call::{build_create, build_call, ExecutionInput, Selector},
+        call::{build_call, build_create, ExecutionInput, Selector},
         DefaultEnvironment,
     };
+
+    use other_contract::OtherContractRef;
 
     #[ink(storage)]
     pub struct CrossContractFlipper {
@@ -24,11 +29,17 @@ mod cross_contract_flipper {
         pub fn new(other_contract_code_hash: Hash) -> Self {
             let other_contract = OtherContractRef::new(true)
                 .code_hash(other_contract_code_hash)
+                // Amount transferred upon the execution of this call (instantiation).
                 .endowment(0)
+                // The salt for determining the hash for the contract account ID.
+                // Use to create multiple instances of the same contract code from the same account.
                 .salt_bytes([0xDE, 0xAD, 0xBE, 0xEF])
                 .instantiate();
 
-            Self { other_contract, other_contract_code_hash }
+            Self {
+                other_contract,
+                other_contract_code_hash,
+            }
         }
 
         /// Initializes a contract by specifying the code hash of the other contract
@@ -40,14 +51,16 @@ mod cross_contract_flipper {
                 .code_hash(other_contract_code_hash)
                 .endowment(0)
                 .exec_input(
-                    ExecutionInput::new(Selector::new(ink::selector_bytes!("new")))
-                        .push_arg(true)
+                    ExecutionInput::new(Selector::new(ink::selector_bytes!("new"))).push_arg(true),
                 )
                 .salt_bytes(&[0xDE, 0xAD, 0xBE, 0xEF])
                 .returns::<OtherContractRef>()
                 .instantiate();
 
-            Self { other_contract, other_contract_code_hash }
+            Self {
+                other_contract,
+                other_contract_code_hash,
+            }
         }
 
         /// Calls the `flip` method of the other contract
@@ -56,28 +69,28 @@ mod cross_contract_flipper {
         #[ink(message)]
         pub fn flip_usig_builder(&mut self) {
             build_call::<DefaultEnvironment>()
-                .call_v1(self.get_other_contract_account_id())
-                .gas_limit(0)
+                .call(self.get_other_contract_account_id())
+                // Amount of funds that are transferred to the other contract with this call.
                 .transferred_value(0)
-                .exec_input(
-                    ExecutionInput::new(Selector::new(ink::selector_bytes!("flip")))
-                )
+                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
+                    "flip"
+                ))))
                 .returns::<()>()
                 .invoke();
         }
 
-        /// Calls the `flip` method of another other contract dynamically
+        /// Calls the `flip` method of another contract dynamically
         /// using the call builder API and specifying the account ID of the other contract
         /// https://docs.rs/ink_env/5.0.0/ink_env/call/struct.CallBuilder.html
+        /// Try without contract dependency
         #[ink(message)]
         pub fn dynamically_flip(&mut self, other_contract_account_id: AccountId) {
             build_call::<DefaultEnvironment>()
-                .call_v1(other_contract_account_id)
-                .gas_limit(0)
+                .call(other_contract_account_id)
                 .transferred_value(0)
-                .exec_input(
-                    ExecutionInput::new(Selector::new(ink::selector_bytes!("flip")))
-                )
+                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
+                    "flip"
+                ))))
                 .returns::<()>()
                 .invoke();
         }
@@ -105,7 +118,6 @@ mod cross_contract_flipper {
 
         /// Calls the `flip` method of the other contract
         /// with the specified weight and storage limits
-        /// using the call builder API.
         #[ink(message)]
         pub fn flip_with_limits(
             &mut self,
@@ -113,9 +125,8 @@ mod cross_contract_flipper {
             proof_size_limit: u64,
             storage_deposit_limit: Balance,
         ) {
-            let call_builder = self.other_contract.call_mut();
-
-            call_builder
+            self.other_contract
+                .call_mut()
                 .flip()
                 .ref_time_limit(ref_time_limit)
                 .proof_size_limit(proof_size_limit)
@@ -125,7 +136,6 @@ mod cross_contract_flipper {
 
         /// Calls the `get` method of the other contract
         /// with the specified weight and storage limits
-        /// using the call builder API.
         #[ink(message)]
         pub fn get_with_limits(
             &mut self,
@@ -133,9 +143,8 @@ mod cross_contract_flipper {
             proof_size_limit: u64,
             storage_deposit_limit: Balance,
         ) -> bool {
-            let call_builder = self.other_contract.call_mut();
-
-            call_builder
+            self.other_contract
+                .call_mut()
                 .get()
                 .ref_time_limit(ref_time_limit)
                 .proof_size_limit(proof_size_limit)
@@ -149,19 +158,16 @@ mod cross_contract_flipper {
         pub fn get_other_contract_account_id(&mut self) -> AccountId {
             self.other_contract.get_account_id()
         }
-        
+
         // Delegate calls `flip` method of the other contract
         // using `DelegateCall` method of the call builder API
         // https://docs.rs/ink_env/5.0.0/ink_env/call/struct.DelegateCall.html
         // https://docs.rs/ink_env/5.0.0/ink_env/call/fn.build_call.html#example-3-delegate-call
+        // https://medium.com/coinmonks/delegatecall-calling-another-contract-function-in-solidity-b579f804178c
         #[ink(message)]
         pub fn delegate_flip(&mut self) {
-            let _ = build_call::<DefaultEnvironment>()
-                .delegate(self.other_contract_code_hash)
-                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!("flip"))))
-                .returns::<()>()
-                .try_invoke();
+            // Bonus Exercise: Delegate call to the other contract
+            todo!()
         }
-        
     }
 }
--- other-contract/lib.rs
+++ other-contract/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 pub use self::other_contract::{
-    OtherContract,
     OtherContractRef,
 };
 
